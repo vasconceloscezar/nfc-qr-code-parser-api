84e27e06d4bf3b4104d3f7143aa2f5ec57832583
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2022-12-08 10:27:58 -0300

    feat: add middlewares adapter and setupMiddleware

---
+++ src/main/config/middleware.ts
@@ -0,0 +1,10 @@
+
+import { Express } from 'express'
+import { bodyParser, contentType, cors } from '../middleware';
+
+
+export default (app: Express): void => {
+	app.use(bodyParser);
+	app.use(cors);
+	app.use(contentType);
+}
