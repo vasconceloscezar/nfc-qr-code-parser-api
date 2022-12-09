54c8659fddede8d0073dd3a668bf63279b1cfaa4
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2022-12-09 10:41:45 -0300

    feat: add / entrypoint route

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,10 @@ import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
+  app.use('/', (req, res) => res.status(200).send({
+    serverDate: new Date(),
+    author: 'Cezar Vasconcelos'
+  }))
   app.use('/api', router)
   readdirSync(join(__dirname, '../routes')).map(async file => {
     if (!file.endsWith('.map')) {
